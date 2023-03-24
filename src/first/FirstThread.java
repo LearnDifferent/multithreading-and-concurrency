f55c289ba4f8febe123745cc30e2901d79e554e7
Zhou <zhou.h.p@outlook.com>
2023-03-24 12:43:02 +0800

    fix: 之前是获取类名，应该改为获取线程名称

--- src/first/FirstThread.java
+++ src/first/FirstThread.java
@@ -15,7 +15,7 @@ public class FirstThread extends Thread {
         int times = 200;
         for (int i = 0; i < times; i++) {
             System.err.println("一共要执行 " + times + " 次 "
-                    + FirstThread.class.getSimpleName()
+                    + Thread.currentThread().getName()
                     + " 线程的 run 方法。当前是第 " + i + " 次");
             try {
                 TimeUnit.MICROSECONDS.sleep(1);
--- src/first/SecondRunnable.java
+++ src/first/SecondRunnable.java
@@ -15,7 +15,7 @@ public class SecondRunnable implements Runnable {
         int times = 200;
         for (int i = 0; i < times; i++) {
             System.err.println("一共要执行 " + times + " 次 "
-                    + FirstThread.class.getSimpleName()
+                    + Thread.currentThread()
                     + " 线程的 run 方法。当前是第 " + i + " 次");
             try {
                 TimeUnit.MICROSECONDS.sleep(1);
